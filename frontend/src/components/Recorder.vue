61933fc0c4590cc81da27f74ce60043993d6a314
rzavodsky <zavodsky.richard1@gmail.com>
2022-03-19 23:21:06 +0100

    Added saving of recorded data

--- frontend/src/components/Recorder.vue
+++ frontend/src/components/Recorder.vue
@@ -1,35 +1,51 @@
 <template>
   <button v-if="!recording" @click="startRecording">Start recording</button>
   <button v-else @click="stopRecording">Stop recording</button>
+  <div v-if="downloadLink !== ''">
+      <audio :src="downloadLink" controls></audio>
+  </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
 const props = defineProps({
-     onError: {
-         type: Function,
-         required: true,
-     }
- });
+    onError: {
+        type: Function,
+        required: true,
+    }
+});
 
 const recording = ref(false);
+const downloadLink = ref("");
 
 let mediaRecorder = null;
+let recordedChunks = [];
 
 function startRecording() {
-  navigator.mediaDevices.getUserMedia({audio: true})
-     .then(stream => {
-         mediaRecorder = new MediaRecorder(stream);
-         recording.value = true;
-     })
-     .catch(err => {
-         props.onError(err.name);
-     });
+    navigator.mediaDevices.getUserMedia({audio: true})
+             .then(stream => {
+                 mediaRecorder = new MediaRecorder(stream);
+                 mediaRecorder.ondataavailable = e => {
+                     recordedChunks.push(e.data);
+                 };
+                 mediaRecorder.onstop = e => {
+                     let blob = new Blob(recordedChunks, { 'type': "audio/ogg; codecs=opus" });
+                     downloadLink.value = URL.createObjectURL(blob);
+                     recordedChunks = [];
+
+                     recording.value = false;
+                 }
+                 mediaRecorder.start();
+
+                 recording.value = true;
+             })
+             .catch(err => {
+                 props.onError(err.name);
+             });
 }
 function stopRecording() {
-  mediaRecorder.stop();
-  mediaRecorder.stream.getAudioTracks().forEach(track => track.stop());
-  recording.value = false;
+    mediaRecorder.stop();
+    mediaRecorder.stream.getAudioTracks().forEach(track => track.stop());
 }
 </script>
