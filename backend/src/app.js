8647c5dc9d2a165ea9becbc3ed6d9a0108f0f808
rzavodsky <zavodsky.richard1@gmail.com>
2022-03-26 19:23:41 +0100

    Added backend endpoint for uploading audio files

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,22 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require("express");
+const multer = require("multer");
 
-app.get('/api', (req, res) => {
-  res.send("Hello world");
+const port = 3000;
+const savedAudioPath = "/home/node/audio";
+
+const app = express();
+const upload = multer({ dest: savedAudioPath, fileFilter: fileFilter });
+
+function fileFilter(req, file, cb) {
+  cb(null, file.mimetype === "audio/ogg");
+}
+
+app.post("/api/upload", upload.single('audioFile'), (req, res, next) => {
+  if (req.file) {
+    res.sendStatus(204);
+  } else {
+    res.sendStatus(400);
+  }
 });
 
 app.listen(port, () => {
